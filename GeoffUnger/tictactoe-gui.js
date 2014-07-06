0fafe31d90a6522a675e8d9c4b71e961192d9ccc
Geoffrey Unger <geoffunger@gmail.com>
2014-07-06 16:57:27 -0700

    Got event handler assignment working for <td> cells

--- GeoffUnger/tictactoe-gui.js
+++ GeoffUnger/tictactoe-gui.js
@@ -1,19 +1,19 @@
-var TicTacToeGui = function() { //module returning constructor:
+var TicTacToeGui = function () { //module returning constructor:
 
     var gameNumber = 0;
     var callback = new Function();
 
-	function Constructor() {
+    function Constructor() {
 
         var gameDiv = document.getElementById("tictactoe");
         var gameTable = document.createElement("table");
         var tableRow, tableCol;
         gameTable.setAttribute("id", "grid" + gameNumber);
         table = gameDiv.appendChild(gameTable);
-        for (var row = 0; row < 3; row++){
+        for (var row = 0; row < 3; row++) {
             tableRow = document.createElement("tr")
             gameTable.appendChild(tableRow);
-            for(col = 0; col < 3; col++){
+            for (col = 0; col < 3; col++) {
                 tableCol = document.createElement("td");
                 tableCol.setAttribute("id", "grid" + gameNumber + "x" + col + "y" + row);
                 tableRow.appendChild(tableCol);
@@ -25,59 +25,65 @@ var TicTacToeGui = function() { //module returning constructor:
 
         gameNumber++;
 
-		this.clear = function(){
+        this.clear = function () {
             var allSquares = document.getElementsByTagName("td");
-            for(var i = 0; i < 9; i++){
+            for (var i = 0; i < 9; i++) {
                 allSquares[i].innerText = " ";
             }
 
         }
-        this.mark = function(xyObj,symbol){
+        this.mark = function (xyObj, symbol) {
             console.log('grid' + this.gameNumber + 'x' + xyObj.x + 'y' + xyObj.y);
             var box = document.getElementById('grid' + this.gameNumber + 'x' + xyObj.x + 'y' + xyObj.y);
             box.innerText = symbol;
         }
 
-        this.setAction = function(callback){
+        this.setAction = function (callback) {
 
-                for(var col = 0; col <3; col++){
-                    for(var row = 0; row < 3; row++){
-                    function assignHandler(rowww, column, game){
-                    var cell = document.getElementById('grid' + game + 'x' + rowww + 'y' + column);
-                    var doit = new Function(callback(rowww, column));
-                    cell.onclick = doit;
+            for (var col = 0; col < 3; col++) {
+                for (var row = 0; row < 3; row++) {
+                    var cell = document.getElementById('grid' + this.gameNumber + 'x' + row + 'y' + col);
+
+                    function callbackMaker(r, c, cb) {
+                        var innerCB = cb;
+                        var innerRow = r;
+                        var innerCol = c;
+                        return function () {
+                            innerCB(innerRow, innerCol);
+                        }
                     }
-                    assignHandler(row,col, this.gameNumber);
+
+                    cell.onclick = callbackMaker(row, col, callback);
                 }
+
             }
         }
 
-        this.getAction = function(){
+        this.getAction = function () {
             return(this.callback);
         }
-	}
 
- 	// more code...
-	return Constructor;	// return TTT gui constructor
+    }
 
-}();
+    return Constructor;	// return TTT gui constructor
+}()
 
 /*var gameNumber = 0;
-var drawHTML = function(){
-    var gameDiv = document.getElementById("tictactoe");
-    var gameTable = document.createElement("table");
-    var tableRow, tableCol;
-    gameTable.setAttribute("id", "grid" + gameNumber);
-    table = gameDiv.appendChild(gameTable);
-    for (var row = 0; row < 3; row++){
-        tableRow = document.createElement("tr")
-        gameTable.appendChild(tableRow);
-        for(col = 0; col < 3; col++){
-            tableCol = document.createElement("td");
-            tableCol.setAttribute("id", "grid" + gameNumber + "x" + col + "y" + row);
-            tableRow.appendChild(tableCol);
+ var drawHTML = function(){
+ var gameDiv = document.getElementById("tictactoe");
+ var gameTable = document.createElement("table");
+ var tableRow, tableCol;
+ gameTable.setAttribute("id", "grid" + gameNumber);
+ table = gameDiv.appendChild(gameTable);
+ for (var row = 0; row < 3; row++){
+ tableRow = document.createElement("tr")
+ gameTable.appendChild(tableRow);
+ for(col = 0; col < 3; col++){
+ tableCol = document.createElement("td");
+ tableCol.setAttribute("id", "grid" + gameNumber + "x" + col + "y" + row);
+ tableRow.appendChild(tableCol);
 
-        }
-    }
-}*/
+ }
+ }
+ }*/
 //window.addEventListener('load', drawHTML);
