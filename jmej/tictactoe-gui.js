7dd0f4d46bac66cd274ac2f723343bf6a5ead1f2
Jesse Mejia <jmejia@mercycorps.org>
2014-07-07 12:34:33 -0700

    gui.clear and gui.mark implemented

--- jmej/tictactoe-gui.js
+++ jmej/tictactoe-gui.js
@@ -3,11 +3,24 @@ var TicTacToeGui = function() { //module returning constructor:
 	// Your code here...
 
 	function Constructor() {
-		this.clear = function (){
-
+		
+		this.clearall = function (){
+			for (var y = 0; y < 3; y++){
+				for (var x = 0; x < 3; x++){
+				var elem = document.getElementById('grid0'+'x'+x+'y'+y)
+				elem.innerHTML = "";
+				}
+			}
 		}
-		this.mark = function(xyObj,symbol){
-			
+
+		this.xytoxyObj = function(x,y) { //takes x and y, returning xyObj that looks like {x:0, y:0}
+		return {'x:'+x,'y'+y};
+		}
+
+		this.mark = function(xyObj,symbol){ //accepts xyObj that looks like {x:0, y:0}
+			var elem = document.getElementById('grid0'+'x'+xyObj.x+'y'+xyObj.y);
+			elem.innerHTML = symbol;
+			elem.classList.add('mark');	
 		} 
 	}
 
