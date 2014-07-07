c768949b7d6fd39f325400e0855577033471a8d5
Tom McCluskey <bosun.tom@gmail.com>
2014-07-07 12:42:18 -0700

    Simpler html working except for class assignments, which cause the script to hang

--- tom/tictactoe-gui.js
+++ tom/tictactoe-gui.js
@@ -1,11 +1,38 @@
 var TicTacToeGui = function() { //module returning constructor:
 
-  this.interpolateId = function(xyObj) {
+  var boardCount = 0;
+
+  function interpolateId(xyObj) {
     var id = 'grid0x' + xyObj.x + 'y' + xyObj.y;
     return id;
-  };
+  }
 
   function Constructor() {
+
+    this.buildBoard = function() {
+      var table = document.createElement('table');
+      var numOfRows = 3;
+      var div = document.getElementById('tictactoe');
+      for(var i = 0; i < numOfRows; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < numOfRows; j++) {
+          var td = document.createElement('td');
+          td.setAttribute('id', 'grid' + boardCount + 'x' + j  + 'y' + i);
+          td.setAttribute('id', 'grid' + boardCount + 'x' + j  + 'y' + i);
+          /*if (i = 0) {
+            td.classList.add('top');
+          }
+          if (j = 0) {
+            td.classList.add('left');
+          }*/
+          tr.appendChild(td);
+        }
+        table.appendChild(tr);
+      }
+      table.setAttribute('id', 'grid' + boardCount);
+      div.appendChild(table);
+      boardCount++;
+    };
 
     this.mark = function(xyObj, symbol) {
       document.getElementById(interpolateId(xyObj)).innerHTML = symbol;
@@ -29,5 +56,5 @@ var TicTacToeGui = function() { //module returning constructor:
   // more code...
   return Constructor;	// return TTT gui constructor
 }();
-var gui = new TicTacToeGui;
+var gui = new TicTacToeGui();
 
